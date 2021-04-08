4a5fb59d7a1f90a6011c1289a127caf32cd07ab5
Darren Zheng <zhengdarren9@gmail.com>
2021-04-07 23:39:14 -0400

    initial commit

---
+++ Snacktacular/Spot.swift
@@ -0,0 +1,101 @@
+//
+//  Spot.swift
+//  Snacktacular
+//
+//  Created by Darren Zheng on 4/4/21.
+//
+
+import Foundation
+import Firebase
+import MapKit
+
+class Spot: NSObject, MKAnnotation {
+    var name: String
+    var address: String
+    var coordinate: CLLocationCoordinate2D
+    var averageRating: Double
+    var numberOfReviews: Int
+    var postingUserID: String
+    var documentID: String
+    
+    var dictionary: [String: Any] {
+        return ["name": name, "address": address, "latitude": latitude, "longtitude": longtitude, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
+    }
+    
+    var latitude: CLLocationDegrees {
+        return coordinate.latitude
+    }
+    
+    var longtitude: CLLocationDegrees {
+        return coordinate.longitude
+    }
+    
+    var title: String? {
+        return name
+    }
+    
+    var subtitle: String? {
+        return address
+    }
+    
+    init(name: String, address: String, coordinate: CLLocationCoordinate2D, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
+        self.name = name
+        self.address = address
+        self.coordinate = coordinate
+        self.averageRating = averageRating
+        self.numberOfReviews = numberOfReviews
+        self.postingUserID = postingUserID
+        self.documentID = documentID
+    }
+    
+    convenience override init() {
+        self.init(name: "", address: "", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
+    }
+    
+    convenience init(dictionary: [String: Any]) {
+        let name = dictionary["name"] as! String? ?? ""
+        let address = dictionary["address"] as! String? ?? ""
+        let latitude = dictionary["latitude"] as! Double? ?? 0.0
+        let longtitude = dictionary["longtitude"] as! Double? ?? 0.0
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        let averageRating = dictionary["averageRating"] as! Double? ?? 0.0
+        let numberOfReviews = dictionary["numberOfReviews"] as! Int? ?? 0
+        let postingUserID = dictionary["postingUserID"] as! String? ?? ""
+        self.init(name: name, address: address, coordinate: coordinate, averageRating: averageRating, numberOfReviews: numberOfReviews, postingUserID: postingUserID, documentID: "")
+    }
+    
+    func saveData(completion: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        // Grab the user ID
+        guard let postingUserID = Auth.auth().currentUser?.uid else {
+            print("ERROR: Could not save data because no valid postingUserID.")
+            return completion(false)
+        }
+        self.postingUserID = postingUserID
+        // Create the dictionary representing data we want to save
+        let dataToSave: [String: Any] = self.dictionary
+        // if we have saved a record, we'll have an ID, otherwise .addDocument will create one.
+        if self.documentID == "" { // Create a new document via .addDocument
+            var ref: DocumentReference? = nil // Firestore
+            ref = db.collection("spots").addDocument(data: dataToSave) { (error) in
+                guard error == nil else {
+                    print("ERROR: adding document \(error!.localizedDescription)")
+                    return completion(false)
+                }
+                self.documentID = ref!.documentID
+                print("Added document: \(self.documentID)")
+                completion(true)
+            }
+        } else { // else save to the existing documentID w/.setData
+            let ref = db.collection("spots").document(self.documentID)
+            ref.setData(dataToSave) { (error) in
+                guard error == nil else {
+                    print("ERROR: updating document \(error!.localizedDescription)")
+                    return completion(false)
+                }
+                print("Updated document: \(self.documentID)")
+                completion(true)
+            }
+        }
+    }
+}
